2b17fcd0a11897ce3a436db9410a5f488f7f4a64
Kevin Brady <kbrady@nist.gov>
2019-04-30 10:44:46 -0400

    Add files via upload

---
+++ query.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+"""
+program: query.py
+description: script to execute a query on a curator.
+"""
+from os.path import join
+from os import listdir
+import requests
+from pprint  import pprint
+import json
+import sys
+import os
+
+def do_query(username='',pwd='',url='', quer=''):
+#
+#Parameters:     username        - username on CDCS instance   (Note:  User must have admin privledges)
+                #password        - password on CDCS instance
+                #url             - url of CDCS instance
+                #quer            - query string
+    
+    query_url = "/explore/common/rest/local-query"
+    turl = url + query_url
+
+    print 'status: Excuting Query.'
+
+    if quer == '': 
+      data = { "query": "{}" }
+    else:
+      data = {'$and': [{'$or': [{'$and': [{'$or': [{\'dict_content.interatomic-potential.key\': \'.*\'},
+                                                   {\'dict_content.interatomic-potential.key.#text\': \'.*\'}]},   
+                                          {'$or': [{\'dict_content.interatomic-potential.keyword\': \'potentials\'},   
+                                                   {\'dict_content.interatomic-potential.keyword.#text\': \'potentials\'}]}]},   
+                                          {'$or': [{\'dict_content.interatomic-potential.element\': \'Au\'},   
+                                                   {\'dict_content.interatomic-potential.element.#text\': \'Au\'}]}]},   \
+                       {'template': {'$in': ['5cc854118e4b1018a1107e00']}}]}
+
+    print "Get:"
+    response = requests.get(turl, data=data, verify=False, auth=(username, pwd))
+    out = response.json()
+    pprint(out)
+    print "Resp: "
+    print response.status_code
+    response_code = response.status_code
+    response_content = json.loads(response.text)
+
+    if response_code == requests.codes.ok:
+       for rec in response_content:
+           pprint(rec)
+           print rec
+    else:
+        response.raise_for_status()
+        raise Exception("- error: a problem occurred when uploading the schema (Error ", response_code, ")")
+
+    print 'status: done.'
+
+
+
+def main(argv=[]):
+    if ( len(argv) != 5 ):
+        print 'usage:   python bulk_upload.py <username> <password> <url> '
+        print 'example: python bulk_upload.py blong my_password http://127.0.0.1:8000 '
+        exit
+
+    args=argv[1:] # skip arg[0]=bulk_upload.py
+
+    username=args[0]
+    pwd=args[1]
+    url=args[2]
+    quer=args[3]
+
+    do_query(username,pwd,url,quer)
+
+if (__name__ == '__main__' ):
+    main(sys.argv)
